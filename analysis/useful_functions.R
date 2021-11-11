8095c14c5d8c9110bfb9ded2bf2f3ed66fb7191b
Aaron Gullickson <aarongullickson@gmail.com>
2021-11-11 12:46:20 -0800

    Move plotting functions to useful functions

--- analysis/useful_functions.R
+++ analysis/useful_functions.R
@@ -2,6 +2,9 @@
 
 #Put any functions that might be used across multiple scripts here so that they can be properly sourced in.
 
+
+# Functions for coding and cleaning ---------------------------------------
+
 code_race <- function(race, hisp) {
   race <- case_when(
     is.na(race) | is.na(hisp) ~ NA_character_,
@@ -127,4 +130,129 @@ is_race_consistent <- function(race_child, hispan_child, race_mom, hispan_mom,
        (!str_detect(race_constructed, "Latino") & hispan_child==0))
 
   return(TF)
+}
+
+
+# Functions for plotting effects ------------------------------------------
+
+plot_effect_scatter <- function(model) {
+  
+  coefs <- tidy(model) %>%
+    filter(str_detect(term, "race") & !str_detect(term, "Multiracial")) %>%
+    mutate(term=str_remove(term, "race")) %>%
+    select(term, estimate, std.error) %>%
+    bind_rows(tibble(term="White", estimate=0, std.error=NA)) %>%
+    mutate(multiracial=str_detect(term, "/"))
+  
+  #ok convert this to have single race groups for each multiracial group
+  #panel and get the assumed midpoint value
+  coef_table <- map(as.list(str_subset(coefs$term, "/")), function(x) {
+    coefs %>%
+      filter(term %in% c(x, str_split(x, "/")[[1]])) %>%
+      mutate(mrace=factor(x,
+                          levels=c("Black/White","White/Indigenous",
+                                   "Black/Indigenous",
+                                   "White/Latino","Black/Latino",
+                                   "Indigenous/Latino",
+                                   "White/Asian","Black/Asian",
+                                   "Indigenous/Asian","Latino/Asian")),
+             estimate_mid=ifelse(multiracial, 
+                                 mean(estimate[!multiracial]),
+                                 estimate),
+             estimate=ifelse(multiracial, estimate_mid+estimate,
+                             estimate))
+  }) %>%
+    bind_rows()
+  
+  ggplot(coef_table, aes(x=estimate_mid, y=estimate,
+                         color=multiracial,
+                         ymin=estimate-1.96*std.error,
+                         ymax=estimate+1.96*std.error))+
+    geom_abline(intercept = 0, slope=1, linetype=1, color="grey80")+
+    geom_hline(data=subset(coef_table, !multiracial),
+               aes(yintercept=estimate), linetype=3, color="grey80")+
+    geom_linerange(alpha=0.5)+
+    geom_point()+
+    geom_text_repel(aes(label=term), size=3)+
+    facet_wrap(~mrace, ncol=3)+
+    theme_bw()+
+    theme(legend.position = "none", panel.grid = element_blank())+
+    scale_color_manual(values=c("grey40","black"))+
+    labs(x="expected log odds ratio of grade retention based on midpoint",
+         y="actual log odds ratio of grade retenion")
+}
+
+plot_effect_point <- function(model) {
+  
+  coefs <- tidy(model) %>%
+    filter(str_detect(term, "race") & !str_detect(term, "Multiracial")) %>%
+    mutate(term=str_remove(term, "race")) %>%
+    select(term, estimate, std.error) %>%
+    bind_rows(tibble(term="White", estimate=0, std.error=NA)) %>%
+    mutate(multiracial=str_detect(term, "/"))
+  
+  #ok convert this to have single race groups for each multiracial group
+  #panel and get the assumed midpoint value
+  coef_table <- map(as.list(str_subset(coefs$term, "/")), function(x) {
+    coefs %>%
+      filter(term %in% c(x, str_split(x, "/")[[1]])) %>%
+      mutate(mrace=factor(x,
+                          levels=c("Black/White",
+                                   "White/Indigenous",
+                                   "Latino/Asian",
+                                   "Black/Latino",
+                                   "White/Latino",
+                                   "Indigenous/Latino",
+                                   "Black/Asian",
+                                   "White/Asian",
+                                   "Indigenous/Asian",
+                                   "Black/Indigenous")),
+             estimate_mid=ifelse(multiracial, 
+                                 mean(estimate[!multiracial]),
+                                 estimate),
+             estimate=ifelse(multiracial, estimate_mid+estimate,
+                             estimate))
+  }) %>%
+    bind_rows()
+  
+  # I want term to be ordered so that multiracial groups are always between two
+  # constituent monoracial groups and monoracial groups to be sorted by the 
+  # magnitude of the estimate option. This is complicated but I should be able
+  # to do this by inserting multiracial groups immediately below the higher 
+  # of the two groups
+  
+  #get terms ordered
+  race_names <- levels(reorder(factor(coef_table$term), coef_table$estimate))
+  #pull out single race and multiple race as separate vectors
+  race_single <- str_subset(race_names, "/", negate=TRUE)
+  race_multi <- str_subset(race_names, "/", negate=FALSE)
+  #start with single races
+  race_full <- race_single
+  #now loop through multiple race names and append based on the higher group
+  for(race in race_multi) {
+    temp <- str_split(race, "/")[[1]]
+    idx <- max(which(race_full==temp[1]), which(race_full==temp[2]))
+    race_full <- append(race_full, race, idx-1)
+  }
+  
+  #now use this as the levels of term as a factor
+  coef_table <- coef_table %>%
+    mutate(term=factor(term, levels=race_full))
+  
+  ggplot(coef_table, aes(x=term, y=estimate,
+                         color=multiracial,
+                         ymin=estimate-1.96*std.error,
+                         ymax=estimate+1.96*std.error))+
+    geom_hline(aes(yintercept=estimate_mid, linetype=multiracial), 
+               color="grey80")+
+    geom_linerange(alpha=0.5)+
+    geom_point()+
+    coord_flip()+
+    facet_wrap(~mrace, ncol=3, scales="free_y")+
+    theme_bw()+
+    theme(legend.position = "none", panel.grid = element_blank())+
+    scale_color_manual(values=c("grey40","black"))+
+    scale_linetype_manual(values=c(3,1))+
+    labs(x=NULL,
+         y="actual log odds ratio of grade retenion")
 }
