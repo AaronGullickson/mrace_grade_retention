ac1d64e849c7ce87ed6bcf24eb167f480284d1c5
Aaron Gullickson <aarongullickson@gmail.com>
2021-11-23 16:32:55 -0800

    Switch analysis.Rmd to robobook html output
    I am using the rmdformats library to output results using the robobook format. This gives me nicer formatting and a dynamic table of contents to get around

--- analysis/check_packages.R
+++ analysis/check_packages.R
@@ -7,7 +7,7 @@
 #add new packages to the chain here
 packages = c("readr","ggplot2","texreg","dplyr","purrr","stringr","broom",
              "ggrepel","purrr", "kableExtra", "tibble","scales","janitor",
-             "survey","modmarg","sandwich","psych")
+             "survey","modmarg","sandwich","psych","rmdformats")
 
 package.check <- lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
