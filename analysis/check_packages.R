ca6d08393e1576e90a5728f296f5e461f04b87aa
Aaron Gullickson <aarongullickson@gmail.com>
2022-09-01 20:26:42 -0700

    Add code chunks to create dummied dataset, run models, and test margins

--- analysis/check_packages.R
+++ analysis/check_packages.R
@@ -8,7 +8,7 @@
 packages = c("readr","ggplot2","texreg","dplyr","purrr","stringr","broom",
              "ggrepel","purrr", "kableExtra", "tibble","scales","janitor",
              "survey","modmarg","sandwich","psych","rmdformats","scales",
-             "fastDummies")
+             "fastDummies","janitor")
 
 package.check <- lapply(packages, FUN = function(x) {
   if (!require(x, character.only = TRUE)) {
